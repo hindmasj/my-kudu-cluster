5f1d06d69b0916270e871f1f6275001df233b10f
Open Badger <open.badger@gmail.com>
2020-12-28 13:34:55 +0300

    Rejigged spark shell image to use the docker_default network and use local names for the master hosts

--- kudu-spark.scala
+++ kudu-spark.scala
@@ -8,10 +8,13 @@ import scala.sys.process._
 sc.setLogLevel("INFO")
 
 //val kuduhost = ("ip addr" #| "awk -f get-ip.awk" !!).trim
-val kuduhost = sys.env("KUDU_IP")
-println(kuduhost)
-val kuduports=Seq(7051,7151,7251)
-val kudumaster=kuduports.map(kuduhost+":"+_).mkString(",")
+// val kuduhost = sys.env("KUDU_IP")
+// println(kuduhost)
+val kuduMasterStem="kudu-master-"
+val kuduMasterArray=for(x <- 1 to 3) yield kuduMasterStem+x
+val kuduPorts=Seq(7051,7151,7251)
+
+val kudumaster=kuduMasterArray.zip(kuduPorts).map(x => s"${x._1}:${x._2}").mkString(",")
 
 val kuduContext = new KuduContext(
 	kudumaster, spark.sparkContext)
--- spark-shell.sh
+++ spark-shell.sh
@@ -4,4 +4,4 @@ LOC=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 . ${LOC}/set-ip.sh
 
-docker run -it --rm -e KUDU_IP=${KUDU_QUICKSTART_IP} sjh/spark
+docker run -it --rm -e KUDU_IP=${KUDU_QUICKSTART_IP} --network docker_default sjh/spark
