ce7c62a461801d4114d9ec338a4e74a3a8c1cbaa
Open Badger <open.badger@gmail.com>
2021-04-24 13:00:39 +0300

    Reorganised the refresh script

--- refresh-images.sh
+++ refresh-images.sh
@@ -4,9 +4,13 @@ LOC=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 . ${LOC}/common.sh
 
 # The cluster relies on a number of images that can be pulled and refreshed at intervals
+USED_IMAGES="${KUDU_IMAGE} ${IMPALA_IMAGE}"
+BUILT_IMAGES="${SPARK_IMAGE} ${ZOOKEEPER_IMAGE} ${KAFKA_IMAGE}"
+
+BASE_IMAGES="${USED_IMAGES} ${SPARK_BASE_IMAGE} ${ZOOKEEPER_BASE_IMAGE} ${KAFKA_BASE_IMAGE}"
+FINAL_IMAGES="${USED_IMAGES} ${BUILT_IMAGES}"
 
 # Base Images
-BASE_IMAGES="${KUDU_IMAGE} ${IMPALA_IMAGE} ${SPARK_BASE_IMAGE} ${ZOOKEEPER_BASE_IMAGE} ${KAFKA_BASE_IMAGE}"
 for image in ${BASE_IMAGES}
 do
     docker pull ${image}
@@ -19,11 +23,13 @@ ${LOC}/kafka/build-images.sh
 docker image prune -f
 
 # Scan final images
-FINAL_IMAGES="${KUDU_IMAGE} ${IMPALA_IMAGE} ${SPARK_IMAGE} ${ZOOKEEPER_IMAGE} ${KAFKA_IMAGE}"
 for image in ${FINAL_IMAGES}
 do
     docker scan ${image}
 done
 
 # Push locally built images
-docker push ${SPARK_IMAGE}
+for image in ${BUILT_IMAGES}
+do
+    docker push ${image}
+done
